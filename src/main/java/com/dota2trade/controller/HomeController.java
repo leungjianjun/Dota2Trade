c7f560c7dbba8eec26f503a0377ed2c65970bbea
leungjianjun <leungjianjun@gmail.com>
2013-01-29 17:18:54 +0800

    login

--- src/main/java/com/dota2trade/controller/HomeController.java
+++ src/main/java/com/dota2trade/controller/HomeController.java
@@ -1,5 +1,10 @@
 package com.dota2trade.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
 /**
  * Created with IntelliJ IDEA.
  * User: ljj-lab
@@ -7,5 +12,12 @@ package com.dota2trade.controller;
  * Time: 下午8:21
  * To change this template use File | Settings | File Templates.
  */
+
+@Controller
 public class HomeController {
+
+    @RequestMapping(value="/index.html", method= RequestMethod.GET)
+    public String home(ModelMap model){
+        return "index";
+    }
 }
--- src/main/java/com/dota2trade/security/SecurityFilterChain.java
+++ src/main/java/com/dota2trade/security/SecurityFilterChain.java
@@ -1,6 +1,7 @@
 package com.dota2trade.security;
 
 import com.dota2trade.dao.UserDao;
+import jcifs.ntlmssp.Type3Message;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 import javax.servlet.Filter;
@@ -23,6 +24,37 @@ public class SecurityFilterChain extends OncePerRequestFilter implements Filter 
     @Override
     protected void doFilterInternal(HttpServletRequest request,HttpServletResponse response, FilterChain chain) throws ServletException, IOException {
         System.out.println("filter work!");
+        String auth = request.getHeader("Authorization");
+        if (auth == null){
+            response.setHeader("WWW-Authenticate", "NTLM");
+            response.setStatus(response.SC_UNAUTHORIZED);
+            response.setContentLength(0) ;
+            response.flushBuffer();
+            return;
+        }
+
+        if (auth.startsWith("NTLM ")){
+            byte[] msg = new sun.misc.BASE64Decoder().decodeBuffer(auth.substring(5));
+            int off = 0, length, offset;
+            if (msg[8] == 1){
+                byte z = 0;
+                byte[] msg1 = {(byte)'N', (byte)'T', (byte)'L', (byte)'M', (byte)'S', (byte)'S', (byte)'P', z,(byte)2, z, z, z, z, z, z, z,(byte)40, z, z, z, (byte)1, (byte)130, z, z,z, (byte)2, (byte)2, (byte)2, z, z, z, z, z, z, z, z, z, z, z, z};
+                response.setHeader("WWW-Authenticate", "NTLM " + new sun.misc.BASE64Encoder().encodeBuffer(msg1));
+                response.setStatus(response.SC_UNAUTHORIZED);
+                response.setContentLength(0) ;
+                response.flushBuffer();
+                return;
+            }else if (msg[8] == 3){
+                //Did Authentication Succeed? All this is always printed.
+
+                Type3Message type3 = new Type3Message(msg);
+
+                System.out.println("osUser: " + type3.getUser());
+                System.out.println("osRemoteHost: + " + type3.getWorkstation());
+                System.out.println("osDomain: " + type3.getDomain());
+
+            }
+        }
         chain.doFilter(request,  response);
     }
 
