c5096be5230fe6ba0c586e5edcc289ce99354571
liyancode <liyan.code@gmail.com>
2013-12-16 17:05:55 +0800

    新添出版社、引用关系方法

--- src/main/java/com/dota2trade/dao/impl/LiteratureDaoImpl.java
+++ src/main/java/com/dota2trade/dao/impl/LiteratureDaoImpl.java
@@ -1,17 +1,20 @@
 package com.dota2trade.dao.impl;
 
 import com.dota2trade.dao.LiteratureDao;
-import com.dota2trade.model.Attachment;
-import com.dota2trade.model.Literature;
-import com.dota2trade.model.LiteratureMeta;
+import com.dota2trade.model.*;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.RowMapper;
 import org.springframework.jdbc.core.namedparam.BeanPropertySqlParameterSource;
 import org.springframework.jdbc.core.namedparam.SqlParameterSource;
 import org.springframework.jdbc.core.support.JdbcDaoSupport;
 import org.springframework.jdbc.support.GeneratedKeyHolder;
 import org.springframework.jdbc.support.KeyHolder;
+import org.springframework.stereotype.Repository;
 
 import javax.sql.DataSource;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.List;
 
 /**
  * Created with IntelliJ IDEA.
@@ -19,6 +22,7 @@ import javax.sql.DataSource;
  * Date: 13-12-15
  * Time: 下午3:36
  */
+@Repository
 public class LiteratureDaoImpl extends JdbcDaoSupport implements LiteratureDao{
 
     @Autowired
@@ -34,19 +38,35 @@ public class LiteratureDaoImpl extends JdbcDaoSupport implements LiteratureDao{
         SqlParameterSource fileParameters = new BeanPropertySqlParameterSource(literature);
 
         KeyHolder keyHolder = new GeneratedKeyHolder();
-
+        //添加文档的属性信息
         int updateCount=this.getJdbcTemplate().update(sql, fileParameters, keyHolder);
 
-        if((updateCount>0)?true:false){
+        if(updateCount>0){
             int keyid= keyHolder.getKey().intValue();
             literature.getLiteratureMeta().setLiteratureid(keyid);
+            //添加文档的基本信息（通用信息）
             boolean addMetaResult=this.addLiteratureMeta(literature.getLiteratureMeta());
             if(addMetaResult==true){
-                literature.getAttachment().setLiteratureid(keyid);
-                boolean addAttachmentResult=this.addAttachment(literature.getAttachment());
-                if(addAttachmentResult==true){
-
-                } else{
+                //添加出版社信息
+                int publisherId= this.addPublisher(literature.getPublisher());
+                if(publisherId!=-1){
+                    //添加文献-出版社关系信息
+                    boolean addLitPubResult=this.addLiteraturePublisher(keyid,publisherId);
+                    if(addLitPubResult==true){
+                        literature.getAttachment().setLiteratureid(keyid);
+                        //添加文档的附件信息
+                        boolean addAttachmentResult=this.addAttachment(literature.getAttachment());
+                        if(addAttachmentResult==true){
+                            //添加引用关系信息
+                            boolean addCiteRelationshipResult=this.addCiteRelationship(literature.getCiteRelationshipList());
+                            return addCiteRelationshipResult;
+                        } else{
+                            return false;
+                        }
+                    }else{
+                        return false;
+                    }
+                }else{
                     return false;
                 }
             }else{
@@ -55,9 +75,6 @@ public class LiteratureDaoImpl extends JdbcDaoSupport implements LiteratureDao{
         }else{
            return false;
         }
-
-
-        return (updateCount>0)?true:false;
     }
 
     @Override
@@ -92,7 +109,7 @@ public class LiteratureDaoImpl extends JdbcDaoSupport implements LiteratureDao{
 
     @Override
     public boolean addAttachment(Attachment attachment) {
-        String sql="INSERT INTO attachment (name,link,creatorid,,literatureid) VALUES (?,?,?,?)";
+        String sql="INSERT INTO attachment (name,link,creatorid,literatureid) VALUES (?,?,?,?)";
         int updateCount=this.getJdbcTemplate().update(
                 sql,
                 attachment.getName(),
@@ -103,5 +120,65 @@ public class LiteratureDaoImpl extends JdbcDaoSupport implements LiteratureDao{
         return (updateCount>0)?true:false;
     }
 
+    @Override
+    public int addPublisher(Publisher publisher) {
+        String selectSql="SELECT * FROM publisher WHERE name=?";
+        //先检查是否有重名的出版社
+        List<Publisher> publishers =  this.getJdbcTemplate().query(selectSql,
+                new Object[]{publisher.getName()},
+                new RowMapper() {
+                    @Override
+                    public Object mapRow(ResultSet rs, int i) throws SQLException {
+                        Publisher p = new Publisher();
+                        p.setId(rs.getInt("id"));
+                        p.setName(rs.getString("name"));
 
+                        return p;  //To change body of implemented methods use File | Settings | File Templates.
+                    }
+                }
+        );
+        if(publishers.size()!=0){//已有同名存在
+            return publishers.get(0).getId();
+        }else{//没有同名，需要新插入一条
+            String sql="INSERT INTO publisher (name) VALUES (:name)";
+
+            SqlParameterSource fileParameters = new BeanPropertySqlParameterSource(publisher);
+
+            KeyHolder keyHolder = new GeneratedKeyHolder();
+
+            int updateCount=this.getJdbcTemplate().update(sql, fileParameters, keyHolder);
+            if(updateCount>0){
+                return keyHolder.getKey().intValue();
+            }else{
+                return -1;
+            }
+        }
+    }
+
+    @Override
+    public boolean addLiteraturePublisher(int literatureid, int publisherid) {
+        String sql="INSERT INTO literature_publisher (literatureid,publisherid) VALUES (?,?)";
+        int updateCount=this.getJdbcTemplate().update(sql,literatureid,publisherid);
+        if(updateCount>0){
+            return true;
+        }else{
+            return false;
+        }
+    }
+
+    @Override
+    public boolean addCiteRelationship(List<CiteRelationship> citeRelationshipList) {
+        String sql="INSERT INTO cited (literatureid,citedbyid,citedtypeid) VALUES (?,?,?)";
+        boolean r=true;
+        for(CiteRelationship cr:citeRelationshipList){
+            int result=this.getJdbcTemplate().update(
+                    sql,
+                    cr.getLiteratureid(),
+                    cr.getCitedbyid(),
+                    cr.getCitedtypeid()
+            );
+            r=(r==true&&result>0);
+        }
+        return r;
+    }
 }
