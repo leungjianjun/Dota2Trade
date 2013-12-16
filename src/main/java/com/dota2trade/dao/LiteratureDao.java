bcc351ce9181eed13dc5e155fa06f6e1c98598ce
liyancode <liyan.code@gmail.com>
2013-12-16 21:02:32 +0800

    方法

--- src/main/java/com/dota2trade/dao/LiteratureDao.java
+++ src/main/java/com/dota2trade/dao/LiteratureDao.java
@@ -11,10 +11,9 @@ import java.util.List;
  * Time: 下午3:30
  */
 public interface LiteratureDao {
+    /*******************add methods***********************/
     /**创建新文献*/
     boolean createLiterature(Literature literature);
-    /**修改已有文献*/
-    boolean updateLiterature(Literature literature);
     /**添加文献基本信息*/
     boolean addLiteratureMeta(LiteratureMeta literatureMeta);
     /**添加文献附件信息*/
@@ -25,4 +24,10 @@ public interface LiteratureDao {
     boolean addLiteraturePublisher(int literatureid,int publisherid);
     /**添加文献的引用关系*/
     boolean addCiteRelationship(List<CiteRelationship> citeRelationshipList);
+
+    /*********************update methods*********************************/
+    /**修改已有文献*/
+    boolean updateLiterature(Literature literature);
+    /**添加文献基本信息*/
+    //boolean updateLiteratureMeta(LiteratureMeta literatureMeta);
 }
