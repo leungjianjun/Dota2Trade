aaa09b8bb4470151461361a30f0ee5b016667709
liyancode <liyan.code@gmail.com>
2013-12-16 13:10:15 +0800

    LiteratureController

--- src/main/java/com/dota2trade/controller/LiteratureController.java
+++ src/main/java/com/dota2trade/controller/LiteratureController.java
@@ -1,10 +1,14 @@
 package com.dota2trade.controller;
 
+import org.springframework.stereotype.Controller;
+
 /**
  * Created with IntelliJ IDEA.
  * User: liyan.code@gmail.com
  * Date: 13-12-15
  * Time: 下午3:05
  */
+@Controller
 public class LiteratureController {
+
 }
