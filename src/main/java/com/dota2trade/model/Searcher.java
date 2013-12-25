e235d7ef78c3726f06e7ccad1601386bc4408f1f
liyancode <liyan.code@gmail.com>
2013-12-25 21:57:49 +0800

    文献检索类

---
+++ src/main/java/com/dota2trade/model/Searcher.java
@@ -0,0 +1,17 @@
+package com.dota2trade.model;
+
+import java.util.List;
+
+/**
+ * Created by liyan.code@gmail.com
+ * Date：13-12-25
+ * Time: 下午9:51
+ *
+ * @version 1.0
+ * 文献检索业务类
+ */
+public class Searcher {
+    public List<Literature> simpleSearch(String keyWord){
+        return null;
+    }
+}
