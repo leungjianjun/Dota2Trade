5f5050aa55361ef64de98271ce15253001f351b5
liyancode <liyan.code@gmail.com>
2013-12-26 10:31:51 +0800

    文献搜索相关的model,放在search包下

---
+++ src/main/java/com/dota2trade/model/search/Searcher.java
@@ -0,0 +1,26 @@
+package com.dota2trade.model.search;
+
+import com.dota2trade.model.Literature;
+import org.apache.lucene.search.IndexSearcher;
+
+import java.util.List;
+
+/**
+ * Created by liyan.code@gmail.com
+ * Date：13-12-25
+ * Time: 下午9:51
+ *
+ * @version 1.0
+ * 文献检索业务类
+ */
+public class Searcher {
+    public List<Literature> simpleSearch(String keyWord){
+        return null;
+    }
+
+    public List<Literature> complexSearch(String keyWord){
+        return null;
+    }
+
+    //IndexSearcher indexSearcher=new IndexSearcher();
+}
