e59a52081b31b463b73da89e1ebff436022d43d6
liyancode <liyan.code@gmail.com>
2013-12-28 15:21:01 +0800

    Impl方法填充完毕

--- src/main/java/com/dota2trade/dao/ConfigDao.java
+++ src/main/java/com/dota2trade/dao/ConfigDao.java
@@ -1,6 +1,7 @@
 package com.dota2trade.dao;
 
 import com.dota2trade.model.Attribute;
+import com.dota2trade.model.LiteraturetypeAttribute;
 
 import java.util.List;
 
@@ -23,17 +24,20 @@ public interface ConfigDao {
      * LiteratureMeta,除此之外，每种文献有自己的属性信息，每个单独的属性为Attribute,
      * 存储时按照单个属性来存
      * */
-    /**添加一个文献类别的一个新属性
+    /**
+     * 添加一个文献类别的一个新属性
      * 顺序是：先查看属性表是否有重名同类型的属性，有则不创建新的属性，没有则创建。
      * 然后向类型-属性关系对应表插入记录，插入的关系默认ismust为1
      * */
     boolean addLiteraturetypeAttribute(int literatureTypeId,Attribute attribute);
+    /**插入一条属性*/
+    int addAttribute(Attribute attribute);
     /**改变类型-属性关系的ismust字段,使得结果与原来相反*/
     boolean changeAttributeIsmust(int literatureTypeId,int attributeId,int ismust);
     /**删除一条类型-属性*/
     boolean deleteLiteraturetypeAttribute(int literatureTypeId,Attribute attribute);
     /**获得一个文献类型的所有已配置属性*/
-    List<Attribute> getAllAttributeOfLiteratureType(int literatureTypeId);
+    List<LiteraturetypeAttribute> getAllAttributeOfLiteratureType(int literatureTypeId);
     /**获得一个文献类型的某一属性类型的已配置属性*/
-    List<Attribute> getOneAttributeOfLiteratureType(int literatureTypeId,int type);
+    List<LiteraturetypeAttribute> getOneAttributeOfLiteratureType(int literatureTypeId,int type);
 }
--- src/main/java/com/dota2trade/dao/impl/ConfigDaoImpl.java
+++ src/main/java/com/dota2trade/dao/impl/ConfigDaoImpl.java
@@ -2,11 +2,18 @@ package com.dota2trade.dao.impl;
 
 import com.dota2trade.dao.ConfigDao;
 import com.dota2trade.model.Attribute;
+import com.dota2trade.model.LiteraturetypeAttribute;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.BeanPropertyRowMapper;
+import org.springframework.jdbc.core.PreparedStatementCreator;
+import org.springframework.jdbc.core.RowMapper;
 import org.springframework.jdbc.core.support.JdbcDaoSupport;
+import org.springframework.jdbc.support.GeneratedKeyHolder;
+import org.springframework.jdbc.support.KeyHolder;
 import org.springframework.stereotype.Repository;
 
 import javax.sql.DataSource;
+import java.sql.*;
 import java.util.List;
 
 /**
@@ -23,37 +30,159 @@ public class ConfigDaoImpl extends JdbcDaoSupport implements ConfigDao {
         setDataSource(dataSource);
     }
     @Override
-    public int addLiteratureType(String name) {
-        return 0;
+    public int addLiteratureType(final String name) {
+        final String sql="INSERT INTO literaturetype (name) VALUES (?)";
+        KeyHolder keyHolder = new GeneratedKeyHolder();
+        PreparedStatementCreator preparedStatementCreator=new PreparedStatementCreator(){
+            public PreparedStatement createPreparedStatement(Connection connection) throws SQLException {
+                PreparedStatement ps = connection.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS);
+                ps.setString(1, name);
+                return ps;
+            }
+        };
+        int updateCount=this.getJdbcTemplate().update(preparedStatementCreator, keyHolder);
+        if(updateCount>0){
+            return keyHolder.getKey().intValue();//取到插入生成的id
+        }else{
+            return -1;
+        }
     }
 
     @Override
     public boolean deleteLiteratureType(int id) {
-        return false;
+        String sql="DELETE FROM literaturetype WHERE id='"+id+"'";
+        int r=this.getJdbcTemplate().update(sql);
+        return (r>0)?true:false;
     }
 
     @Override
     public boolean addLiteraturetypeAttribute(int literatureTypeId, Attribute attribute) {
-        return false;
+        int attributeid=this.addAttribute(attribute);
+        String sql="INSERT INTO literaturetype_attribute(literaturetypeid," +
+                "attributeid,attributetype,ismust) VALUES(?,?,?,?)";
+        int r=this.getJdbcTemplate().update(sql,literatureTypeId,
+                attributeid,attribute.getType(),1);
+
+        return (r>0)?true:false;
+    }
+
+    @Override
+    public int addAttribute(final Attribute attribute) {
+        String selectSql="SELECT * FROM attribute WHERE name=? AND type=?";
+        //先检查是否有重名的
+        List<Attribute> attributes =  this.getJdbcTemplate().query(selectSql,
+                new Object[]{attribute.getName(),attribute.getType()},
+                new RowMapper() {
+                    @Override
+                    public Object mapRow(ResultSet rs, int i) throws SQLException {
+                        Attribute a = new Attribute();
+                        a.setId(rs.getInt("id"));
+                        a.setName(rs.getString("name"));
+                        a.setType(rs.getInt("type"));
+
+                        return a;
+                    }
+                }
+        );
+
+        if(attributes.size()!=0){//已有同名存在
+            return attributes.get(0).getId();
+        }else{//没有同名，需要新插入一条
+            final String sql="INSERT INTO attribute (name,type) VALUES (?,?)";
+            KeyHolder keyHolder = new GeneratedKeyHolder();
+
+            PreparedStatementCreator preparedStatementCreator=new PreparedStatementCreator(){
+                public PreparedStatement createPreparedStatement(Connection connection) throws SQLException {
+                    PreparedStatement ps = connection.prepareStatement(sql,Statement.RETURN_GENERATED_KEYS);
+                    ps.setString(1, attribute.getName());
+                    ps.setInt(2, attribute.getType());
+                    return ps;
+                }
+            };
+            int updateCount=this.getJdbcTemplate().update(preparedStatementCreator,keyHolder);
+            if(updateCount>0){
+                return keyHolder.getKey().intValue();
+            }else{
+                return -1;
+            }
+        }
     }
 
     @Override
     public boolean changeAttributeIsmust(int literatureTypeId, int attributeId, int ismust) {
-        return false;
+        String sql="UPDATE literaturetype_attribute SET ismust='"+ismust+"' " +
+                "WHERE literaturetypeid='"+literatureTypeId+"' AND attributeid='"+attributeId+"'";
+        int r=this.getJdbcTemplate().update(sql);
+        return (r>0)?true:false;
     }
 
     @Override
     public boolean deleteLiteraturetypeAttribute(int literatureTypeId, Attribute attribute) {
-        return false;
+        String sql="DELETE FROM literaturetype_attribute " +
+                "WHERE literaturetypeid='"+literatureTypeId+"' AND attributeid='"+attribute.getId()+"'";
+        int r=this.getJdbcTemplate().update(sql);
+        return (r>0)?true:false;
     }
 
     @Override
-    public List<Attribute> getAllAttributeOfLiteratureType(int literatureTypeId) {
-        return null;
+    public List<LiteraturetypeAttribute> getAllAttributeOfLiteratureType(int literatureTypeId) {
+        String selectSql="SELECT * FROM attribute WHERE id IN ( " +
+                "SELECT attributeid FROM literaturetype_attribute WHERE literaturetypeid='"+literatureTypeId+"')";
+
+        List<Attribute> attributeList=this.getJdbcTemplate().query(selectSql,new BeanPropertyRowMapper(Attribute.class));
+
+        String sql="SELECT * FROM literaturetype_attribute WHERE literaturetypeid='"+literatureTypeId+"'";
+        List<LiteraturetypeAttribute> literaturetypeAttributeList =  this.getJdbcTemplate().query(sql,
+                new RowMapper() {
+                    @Override
+                    public Object mapRow(ResultSet rs, int i) throws SQLException {
+                        LiteraturetypeAttribute literaturetypeAttribute = new LiteraturetypeAttribute();
+                        literaturetypeAttribute.setIsmust(rs.getInt("ismust"));
+                        return literaturetypeAttribute;
+                    }
+                }
+        );
+        int size=literaturetypeAttributeList.size();
+        if(attributeList.size()==size){
+            for(int i=0;i<size;i++){
+                literaturetypeAttributeList.get(i).setLiteraturetypeid(literatureTypeId);
+                literaturetypeAttributeList.get(i).setAttribute(attributeList.get(i));
+            }
+            return literaturetypeAttributeList;
+        }else{
+            return null;
+        }
     }
 
     @Override
-    public List<Attribute> getOneAttributeOfLiteratureType(int literatureTypeId, int type) {
-        return null;
+    public List<LiteraturetypeAttribute> getOneAttributeOfLiteratureType(int literatureTypeId, int type) {
+        String selectSql="SELECT * FROM attribute WHERE id IN ( " +
+                "SELECT attributeid FROM literaturetype_attribute " +
+                "WHERE literaturetypeid='"+literatureTypeId+"' AND attributetype='"+type+"')";
+
+        List<Attribute> attributeList=this.getJdbcTemplate().query(selectSql,new BeanPropertyRowMapper(Attribute.class));
+
+        String sql="SELECT * FROM literaturetype_attribute " +
+                "WHERE literaturetypeid='"+literatureTypeId+"' AND attributetype='"+type+"'";
+        List<LiteraturetypeAttribute> literaturetypeAttributeList =  this.getJdbcTemplate().query(sql,
+                new RowMapper() {
+                    @Override
+                    public Object mapRow(ResultSet rs, int i) throws SQLException {
+                        LiteraturetypeAttribute literaturetypeAttribute = new LiteraturetypeAttribute();
+                        literaturetypeAttribute.setIsmust(rs.getInt("ismust"));
+                        return literaturetypeAttribute;
+                    }
+                }
+        );
+        int size=literaturetypeAttributeList.size();
+        if(attributeList.size()==size){
+            for(int i=0;i<size;i++){
+                literaturetypeAttributeList.get(i).setLiteraturetypeid(literatureTypeId);
+                literaturetypeAttributeList.get(i).setAttribute(attributeList.get(i));
+            }
+            return literaturetypeAttributeList;
+        }else{
+            return null;
+        }
     }
 }
