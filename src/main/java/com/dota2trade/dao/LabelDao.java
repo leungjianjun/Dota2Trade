7a63be98b4f892f5b3c8e5220167446e450e5794
丁玲娟 <teireien@teireienmatoMacBook-Pro.local>
2014-01-01 20:07:12 +0800

    添加标签

---
+++ src/main/java/com/dota2trade/dao/LabelDao.java
@@ -0,0 +1,26 @@
+package com.dota2trade.dao;
+
+import com.dota2trade.model.Label;
+import com.dota2trade.model.LabelLiterature;
+import com.dota2trade.model.Literature;
+
+import java.util.List;
+
+/**
+ * Created by teireien on 14-1-1.
+ */
+public interface LabelDao {
+    /** 添加新标签*/
+    public int addLabel(Label label);
+    public boolean addLiteratureLabel(LabelLiterature labelLiterature);
+    /**根据labelid获得label*/
+    public Label getLabelByid(int id);
+    /**根据literatureid获取标签*/
+    public List<Label> getLabelListByLiteratureId(int literatureid);
+    /**根据userid获得他用过的标签*/
+    public List<Label> getLabelListByuserId(int userid);
+    /**根据labelid获得所有使用该label的literature*/
+    public List<LabelLiterature> getLiteratureListByLabelId(int labelid);
+    /**获得常用标签*/
+    public List<Label> getCommonLabelList();
+}
