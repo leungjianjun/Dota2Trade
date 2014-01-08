5794110067506287568dbf53d26f5db503e96d97
丁玲娟 <teireien@teireienmatoMacBook-Pro.local>
2014-01-08 18:39:49 +0800

    logHelper

--- src/main/java/com/dota2trade/util/LogHelper.java
+++ src/main/java/com/dota2trade/util/LogHelper.java
@@ -2,11 +2,7 @@ package com.dota2trade.util;
 
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
-
-<<<<<<< HEAD
-=======
 import java.text.DateFormat;
->>>>>>> 8ce6d6aea5a12bac2d737053311791047924fd78
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
 
@@ -22,21 +18,8 @@ public class LogHelper {
 
     public static void addLog(String name, String action){
         Calendar cal = Calendar.getInstance();
-<<<<<<< HEAD
-        SimpleDateFormat format1 = new SimpleDateFormat("yyyy年MM月dd日 HH:mm");
-        log.warn("<&&"+format1.format(cal.getTime())+"&&"+name+"&&"+action+"&&>");
-    }
-
-    public static void main(String[] args){
-        Calendar cal = Calendar.getInstance();
-        SimpleDateFormat format1 = new SimpleDateFormat("yyyy年MM月dd日 HH:mm");
-
-        System.out.println(format1.format(cal.getTime()));
-=======
         cal.getTime();
         SimpleDateFormat format=new SimpleDateFormat("HH:mm");
-
         log.warn("<&&"+DateFormat.getDateInstance(DateFormat.FULL).format(cal.getTime())+"&&"+format.format(cal.getTime())+"&&"+name+"&&"+action+"&&>");
->>>>>>> 8ce6d6aea5a12bac2d737053311791047924fd78
     }
 }
