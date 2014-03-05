150f5ddbd4efd5b8bb44bfb90d616d5ae29b3ad1
丁玲娟 <teireien@teireienmatoMacBook-Pro.local>
2014-03-05 21:49:44 +0800

    hdfs

--- src/main/java/com/dota2trade/util/FileUploadHelper.java
+++ src/main/java/com/dota2trade/util/FileUploadHelper.java
@@ -3,6 +3,15 @@ package com.dota2trade.util;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.*;
+import java.net.URI;
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.FSDataInputStream;
+import org.apache.hadoop.fs.FSDataOutputStream;
+import org.apache.hadoop.fs.FileStatus;
+import org.apache.hadoop.fs.FileSystem;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.IOUtils;
+import org.apache.hadoop.util.Progressable;
 
 /**
  * Created with IntelliJ IDEA.
@@ -14,23 +23,25 @@ public class FileUploadHelper {
     /**
      * @param type 附件类型：paper or other
      * */
-    public static void uploadFile(MultipartFile file, String filename,String type) throws IOException {
+    public static void uploadFile(MultipartFile file, String filename,String type)
+            throws IOException {
         String dir;
         if(type.equals("paper")){
             dir="attachment/paper";
         }else{
             dir="attachment/other";
         }
-        File newFile = new File(dir,filename);
-        newFile.createNewFile();
-        OutputStream outputStream = new FileOutputStream(newFile);
-        InputStream inputStream = file.getInputStream();
-        int read = 0;
-        byte[] bytes = new byte[1024];
+        dir = "hdfs://localhost:9000/"+dir+filename;
+        Configuration conf = new Configuration();
 
-        while ((read = inputStream.read(bytes)) != -1) {
-            outputStream.write(bytes, 0, read);
-        }
+        InputStream inputStream = file.getInputStream();
+        FileSystem fs = FileSystem.get(URI.create(dir), conf);
+        OutputStream outputStream = fs.create(new Path(dir), new Progressable() {
+            public void progress() {
+                System.out.print(".");
+            }
+        });
+        IOUtils.copyBytes(inputStream, outputStream, 4096, true);
         outputStream.close();
         inputStream.close();
     }
